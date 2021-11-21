27ac752cdc92b3de1fdbdccfdb0c9a38e92c55ab
Rock-it-science <wilmc17@gmail.com>
2021-11-20 23:19:35 -0800

    Update search link base

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import csv
 from datetime import datetime
 import json
+import pandas as pd
 
 from search_ads import search
 from compile import compile_raw
 from process import process_data
 
-search_term = "Software Developer"
+search_term = "Manager"
 location = "Vancouver, BC"
 
 # Iterate through pages until attempt to access page index returns an error
@@ -14,7 +15,7 @@ ads_dict = {}
 try:
     i = 0
     while True:
-        url = 'https://ca.indeed.com/jobs?q=' + search_term + '&l=' + location + '&radius=1000&limit=50&filter=0&start='+str(i)
+        url = 'https://ca.indeed.com/jobs?q=' + search_term + '&l=' + location + '&radius=1000&sort=date&limit=50&filter=0&start='+str(i)
         ads_dict = search(url)
         print('Searching ', url)
         print(str(len(ads_dict)), ' pages scanned')
@@ -26,10 +27,21 @@ except Exception as e:
     with open('raw_data/job ads-' + search_term + '-' + datetime.today().strftime('%Y-%m-%d_%H_%M') + '.json', 'a', encoding='utf-8') as f:
         json.dump(ads_dict, f, ensure_ascii=False, indent=4)
     
+    # Get number of jobs before we add new ones
+    df = pd.read_csv("processed.csv")
+    prior_size = len(df.index)
+
     # Compile and process all results
     print('Stopped with exception: \"', e, '\"')
     print('Finished gathering data, now updating raw.csv')
     compile_raw()
     print('Updated raw.csv, now updating processed.csv')
     process_data()
+
+    # Get size number of jobs after we have added new ones
+    df = pd.read_csv("processed.csv")
+    new_size = len(df.index)
+
+    print(new_size - prior_size, " new jobs added to processed.csv")
+
     print('Finished')
